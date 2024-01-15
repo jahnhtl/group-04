e151234d1b7e99c365ef08cecef9951355fafb84
HTLW10\001152 <190204@htlw10.at>
2024-01-15 12:44:22 +0100

    Update Code 15.01.2024 Sensorwerte links und Rechts einlesbar

--- Car_project_MUS_KOEK/Car_project_MUS_KOEK.ino
+++ Car_project_MUS_KOEK/Car_project_MUS_KOEK.ino
@@ -1,5 +1,103 @@
-
 int IN1 = 4;
+int IN2 = 7;
+int IN3 = 5;
+int IN4 = 6; 
+int StartButton = 8;
+int StopButton = 9;
+int LeftSensor = A0;  // Analog input pin for the left sensor
+int RightSensor = A1; // Analog input pin for the right sensor
+int StartButtonState;
+int StopButtonState;
+int leftDistanceThreshold = 30;  // Adjust this threshold based on your needs for the left sensor
+int rightDistanceThreshold = 30; // Adjust this threshold based on your needs for the right sensor
+
+void setup()
+{
+  // Set the output pins
+  Serial.begin(115200);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(StartButton, INPUT_PULLUP);
+  pinMode(StopButton, INPUT_PULLUP);
+}
+
+void loop()
+{
+  StartButtonState = digitalRead(StartButton);
+  StopButtonState = digitalRead(StopButton);
+  
+  int leftDistance = analogRead(LeftSensor);
+  int rightDistance = analogRead(RightSensor);
+
+  if (StartButtonState == LOW && leftDistance > leftDistanceThreshold && rightDistance > rightDistanceThreshold) {
+    // Start the motors
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 255);
+    analogWrite(IN3, 255);
+    analogWrite(IN4, 0);
+  } else {
+    // Stop the motors
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 0);
+    analogWrite(IN3, 0);
+    analogWrite(IN4, 0);
+  }
+
+  if (StopButtonState == LOW) {
+    // Stop the motors
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 0);
+    analogWrite(IN3, 0);
+    analogWrite(IN4, 0);
+  }
+
+  // Print the distances for debugging
+  Serial.print("Left Distance: ");
+  Serial.print(leftDistance);
+  Serial.print(" | Right Distance: ");
+  Serial.println(rightDistance);
+  
+  delay(100);  // Adjust delay as needed
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int IN1 = 4;
 int IN2 = 7;
 int IN3 = 5;
 int IN4 = 6; 
@@ -27,10 +125,10 @@ void loop()
 
   if (StartButtonState == LOW) {
     // Starte die Motoren
-    analogWrite(IN1, 255);
-    analogWrite(IN2, 0);
-    analogWrite(IN3, 0);
-    analogWrite(IN4, 255);
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 255);
+    analogWrite(IN3, 255);
+    analogWrite(IN4, 0);
     //delay(5000);
   }
 
@@ -41,9 +139,6 @@ void loop()
     analogWrite(IN3, 0);
     analogWrite(IN4, 0);
     //delay(500);
-  }
-
-
-
-  
+  }  
 }
+*/
