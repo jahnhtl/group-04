0d7aa65acfd99fcc221a912ecba90bfc57412885
HTLW10\001152 <190204@htlw10.at>
2024-01-22 12:57:52 +0100

    Update Code 22.01.2024 Changelog: Sensors in cm and better understanding of the sensors

--- Car_project_MUS_KOEK/Car_project_MUS_KOEK.ino
+++ Car_project_MUS_KOEK/Car_project_MUS_KOEK.ino
@@ -1,15 +1,18 @@
+#include <SharpIR.h>
+
 int IN1 = 4;
 int IN2 = 7;
 int IN3 = 5;
 int IN4 = 6; 
 int StartButton = 8;
 int StopButton = 9;
-int LeftSensor = A0;  // Analog input pin for the left sensor
-int RightSensor = A1; // Analog input pin for the right sensor
+int LeftSensor = A1;
+int RightSensor = A0;
 int StartButtonState;
 int StopButtonState;
-int leftDistanceThreshold = 30;  // Adjust this threshold based on your needs for the left sensor
-int rightDistanceThreshold = 30; // Adjust this threshold based on your needs for the right sensor
+
+SharpIR LeftDD(SharpIR::GP2Y0A21YK0F, A1);
+SharpIR RightDD(SharpIR::GP2Y0A21YK0F, A0);
 
 void setup()
 {
@@ -25,120 +28,58 @@ void setup()
 
 void loop()
 {
+  static int fahren = 0;
   StartButtonState = digitalRead(StartButton);
   StopButtonState = digitalRead(StopButton);
-  
-  int leftDistance = analogRead(LeftSensor);
-  int rightDistance = analogRead(RightSensor);
 
-  if (StartButtonState == LOW && leftDistance > leftDistanceThreshold && rightDistance > rightDistanceThreshold) {
-    // Start the motors
+  //int leftDistance = analogRead(LeftSensor);
+  //int rightDistance = analogRead(RightSensor);
+
+  int leftDistance = LeftDD.getDistance();
+  int rightDistance = RightDD.getDistance();
+
+  if (StartButtonState == LOW) {
+    fahren = 1;
+  }
+  if (StopButtonState == LOW) {
+    fahren = 0;
+  }
+
+  if(fahren == 1)
+  {
+    // Starte die Motoren
     analogWrite(IN1, 0);
     analogWrite(IN2, 255);
     analogWrite(IN3, 255);
     analogWrite(IN4, 0);
-  } else {
-    // Stop the motors
+    //delay(5000);  
+  if ( LeftSensor > RightSensor ) {
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 50);
+    analogWrite(IN3, 255);
+    analogWrite(IN4, 0);
+  }  if ( LeftSensor < RightSensor ) {
+    analogWrite(IN1, 0);
+    analogWrite(IN2, 50);
+    analogWrite(IN3, 255);
+    analogWrite(IN4, 0);
+  }
+
+  }
+  else
+  {
     analogWrite(IN1, 0);
     analogWrite(IN2, 0);
     analogWrite(IN3, 0);
     analogWrite(IN4, 0);
   }
 
-  if (StopButtonState == LOW) {
-    // Stop the motors
-    analogWrite(IN1, 0);
-    analogWrite(IN2, 0);
-    analogWrite(IN3, 0);
-    analogWrite(IN4, 0);
-  }
 
-  // Print the distances for debugging
+
   Serial.print("Left Distance: ");
   Serial.print(leftDistance);
   Serial.print(" | Right Distance: ");
   Serial.println(rightDistance);
   
   delay(100);  // Adjust delay as needed
-}
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int IN1 = 4;
-int IN2 = 7;
-int IN3 = 5;
-int IN4 = 6; 
-int StartButton = 8;
-int StopButton = 9;
-int StartButtonState;
-int StopButtonState;
-
-void setup()
-{
-  // Set the output pins
-  Serial.begin(115200);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(StartButton, INPUT_PULLUP);
-  pinMode(StopButton, INPUT_PULLUP);
-}
-
-void loop()
-{
-  StartButtonState = digitalRead(StartButton);
-  StopButtonState = digitalRead(StopButton);
-
-  if (StartButtonState == LOW) {
-    // Starte die Motoren
-    analogWrite(IN1, 0);
-    analogWrite(IN2, 255);
-    analogWrite(IN3, 255);
-    analogWrite(IN4, 0);
-    //delay(5000);
-  }
-
-  if (StopButtonState == LOW) {
-    // Stoppe die Motoren
-    analogWrite(IN1, 0);
-    analogWrite(IN2, 0);
-    analogWrite(IN3, 0);
-    analogWrite(IN4, 0);
-    //delay(500);
-  }  
-}
-*/
+}  
